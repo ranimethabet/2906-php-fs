8fe7f49c8d322474930b867d7d26e04fa8bf235a
Ismail <Ismail.Abokhalifa2002@gmail.com>
2025-01-17 23:32:34 +0200

    watermelon cut scene

--- index.php
+++ index.php
@@ -4,9 +4,42 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="icon" href="work.jfif">
+    <style>
+        body{
+            background-color: #ccf;
+        }
+        p{
+            color: blue;
+            font-size: 20px;
+        }
+        button{
+            border-radius: 20px;
+            padding: 20px 30px;
+            background-color: green;
+            color: yellow;
+            font-size: 40px;
+            cursor: pointer;
+        }
+        h4{
+            font-size: 25px;
+        }
+    </style>
 </head>
 <body>
     <h1>Home Page</h1>
-    <h1>karim diab</h1>
+    <h1>Ismail Abokhalifa</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius excepturi quis nostrum fuga! Libero, sunt quibusdam rem incidunt qui officia sequi facere nesciunt laborum sit atque dicta vel laudantium perferendis repudiandae illum amet a temporibus sapiente minima! Saepe a harum, placeat culpa ipsa repellat velit ab eius eos accusantium tempora cupiditate atque quidem accusamus dolor, nihil adipisci nostrum, voluptatibus deleniti! Repudiandae tenetur neque impedit beatae placeat? Fugiat, quos sit sunt iure excepturi eveniet ipsam delectus fugit eius ratione omnis mollitia. Fugit quas eum aspernatur fuga repellat animi consequatur libero, esse delectus maiores eaque atque consequuntur sequi saepe tempore magni accusamus.</p>
+
+    <?php
+        echo "<h4>I am Ismail and I'm gonna show you kind of interesting ideas</h4>";
+    ?>
+    <br />
+    <br />
+    <br />
+    <a href="watermelon.html"><button>Move Me</button></a>
+
+
+
 </body>
 </html>
