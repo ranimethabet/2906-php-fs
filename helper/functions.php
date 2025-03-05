6cf2a7f92e010a7fa0a5088a9742ff88e045acd5
ranimethabet <ranimethabet@gmail.com>
2025-03-05 22:47:29 +0200

    practice 16

---
+++ helper/functions.php
@@ -0,0 +1,11 @@
+<?php
+function dd($item)
+{
+var_dump($item);
+die;
+};
+
+function dump($item)
+{
+    var_dump($item);
+};
---
+++ required_data.php
@@ -0,0 +1,9 @@
+<?php
+require_once('config/config.php');
+
+require_once('helper/functions.php');
+
+
+$db=new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
+
+//dd($db);
